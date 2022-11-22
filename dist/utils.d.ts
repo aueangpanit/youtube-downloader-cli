d596d25413c086999346ce421f24652ab9a415c2
Aue Angpanitcharoen <aueangpanit@gmail.com>
2022-11-22 15:32:41 +0000

    clean up dist folder + add to gitignore

--- dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import ytdl from 'ytdl-core';
-export declare function getOutputFileName(info: ytdl.videoInfo): string;
-export declare function processVideo(url: string, outputFileName: string, thumbnail_url: string): Promise<unknown>;
-export declare function processImage(url: string, outputFileName: string): Promise<void>;
